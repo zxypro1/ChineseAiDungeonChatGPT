d125849e18adeaa4e3a936f2b0268864d46ffecb
zxypro <1018995004@qq.com>
2022-12-10 21:13:33 +0800

    Init

---
+++ dnd_engine/dnd_config.py
@@ -0,0 +1,10 @@
+dnd_config = {
+    "character": {
+        "strength": 10,
+        "dexterity": 10,
+        "constitution": 10,
+        "intelligence": 10,
+        "wisdom": 10,
+        "charisma": 10
+    }
+}
